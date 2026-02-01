46001dccafdff89d8e4e09479fec5da89711cd20
roshiniroshini85601-dotcom <roshiniroshini85601@gmail.com>
2026-02-01 23:13:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
-function openmenu(){
+// ===== EMAILJS INITIALIZATION =====
+(function () {
+  emailjs.init("QBSBRscNn79JshTtZ"); // EmailJS Public Key
+})();
+
+// ===== MENU =====
+function openmenu() {
   document.getElementById("sidemenu").style.right = "0";
 }
 
-function closemenu(){
+function closemenu() {
   document.getElementById("sidemenu").style.right = "-200px";
 }
 
-function opentab(tabname) {
-  const tablinks = document.getElementsByClassName("tl");
-  const tabcontents = document.getElementsByClassName("titlecontent");
+// ===== SEND EMAIL =====
+function sendEmail(event) {
+  event.preventDefault();
 
-  for (let tab of tablinks) tab.classList.remove("active");
-  for (let content of tabcontents) content.classList.remove("active-title");
+  window.alert("Sending your message...");
 
-  event.currentTarget.classList.add("active");
-  document.getElementById(tabname).classList.add("active-title");
+  const params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  };
+
+  // Send mail to YOU
+  emailjs.send("service_2mesjcl", "TEMPLATE_ADMIN", params)
+    .then(() => {
+
+      // Auto reply to USER
+      emailjs.send("service_2mesjcl", "TEMPLATE_USER", params);
+
+      window.alert(
+        "Thank you for contacting me!\n\n" +
+        "Your message has been sent successfully.\n" +
+        "A confirmation email has been sent to your email address."
+      );
+
+      document.querySelector("form").reset();
+    })
+    .catch(error => {
+      window.alert(
+        "Oops! Something went wrong.\n" +
+        "Please try again later."
+      );
+      console.error(error);
+    });
 }
